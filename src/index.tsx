f8b526ea4f0f0aff62cda85c309190de7797bef8
fleeri91 <fleeri91@gmail.com>
2020-10-07 23:36:00 +0200

    ...

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// --------------------------------------------------
+// Library Imports
+// --------------------------------------------------
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 
+// --------------------------------------------------
+// Style Imports
+// --------------------------------------------------
+import './index.css';
+
+// --------------------------------------------------
+// Components Imports
+// --------------------------------------------------
+import App from './App';
+
+// --------------------------------------------------
+// DOM Render
+// --------------------------------------------------
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
